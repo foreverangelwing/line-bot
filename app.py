ab933f26711feea26df76cc25281bce711e510c6
foreverangelwing <foreverangelwing@gmail.com>
2021-08-07 04:52:23 +0800

    fix image_message

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ def handle_message(event):
 
     if msg in ['UR-515', 'UR515', 'ur515', 'ur-515']:
         image_message = ImageSendMessage(
-        original_content_url='https://bit.ly/3ywwqqT',
-        preview_image_url='https://bit.ly/3ywwqqT'
+        original_content_url='https://bit.ly/3fEkpIw',
+        preview_image_url='https://bit.ly/3fEkpIw'
     )
         line_bot_api.reply_message(
         event.reply_token,
