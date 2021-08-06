99a7c24781f29603119d7013a2d0fda5ec7501d1
foreverangelwing <foreverangelwing@gmail.com>
2021-08-07 01:09:09 +0800

    add sticker_message

--- app.py
+++ app.py
@@ -40,6 +40,18 @@ def handle_message(event):
     msg = event.message.text
     r = '很抱歉，我不懂您在說什麼？'
 
+    if '給我貼圖' in msg:
+        sticker_message = StickerSendMessage(
+        package_id='1',
+        sticker_id='1'
+    )
+
+        line_bot_api.reply_message(
+        event.reply_token,
+        sticker_message)
+
+        return
+
     if msg in ['hi', 'Hi']:
         r = 'hi'
     elif msg == '你吃飯了嗎？':
